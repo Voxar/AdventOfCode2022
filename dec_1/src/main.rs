c334410d33903959b87fe7bc6e9476b5a589d361
Patrik Sjöberg <voxxar@gmail.com>
2022-12-01 13:11:59 +0100

    part2

--- dec_1/src/main.rs
+++ dec_1/src/main.rs
@@ -1,14 +1,35 @@
 // https://adventofcode.com/2022/day/1
 
+fn part_one() -> u32 {
+    let top_1 = std::fs::read_to_string("input.txt")
+        .expect("Shoud read the input file")
+        .lines()
+        .fold((0, 0), |(max, cur), line| match line.parse::<u32>() {
+            Ok(val) => (max, cur + val),
+            Err(_) => (if max > cur { max } else { cur }, 0),
+        }).0;
+    println!("The fattest elf has {} calories.", top_1);
+    top_1
+}
+
+fn part_two() -> u32 {
+    let mut elfs = std::fs::read_to_string("input.txt")
+        .expect("Shoud read the input file")
+        .lines()
+        .fold((Vec::new(), 0), |(mut elfs, cur), line| match line.parse::<u32>() {
+            Ok(val) => (elfs, cur + val),
+            Err(_) => {
+                elfs.push(cur);
+                (elfs, 0)
+            }
+        }).0;
+    elfs.sort_by(|a, b| b.cmp(a) );
+    let top_3 = elfs.split_at(3).0.iter().fold(0, |a, b| a + b);
+    println!("The three fattest elfs together has {:?} calories.", top_3);
+    top_3
+}
+
 fn main() {
-    println!(
-        "The fattest elf has {} calories.",
-        std::fs::read_to_string("input.txt")
-            .expect("Shoud read the input file")
-            .lines()
-            .fold((0, 0), |(max, cur), line| match line.parse::<u32>() {
-                Ok(val) => (max, cur + val),
-                Err(_) => (if max > cur { max } else { cur }, 0),
-            }).0
-    );
+    assert_eq!(part_one(), 68787);
+    assert_eq!(part_two(), 198041);
 }
