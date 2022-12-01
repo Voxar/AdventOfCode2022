755c6ff97eacf5e4c007ba9f973961b717e73c13
Patrik Sjöberg <voxxar@gmail.com>
2022-12-01 16:40:12 +0100

    Refactor into chunky onetime parse

--- dec_1/src/main.rs
+++ dec_1/src/main.rs
@@ -2,35 +2,34 @@
 
 
 fn main() {
-    assert_eq!(part_one(), 68787);
-    assert_eq!(part_two(), 198041);
+    let input = std::fs::read_to_string("input.txt")
+        .expect("Shoud read the input file")
+        .split("\n\n")
+        .map( |chunk| chunk
+            .lines()
+            .map(|line| line.parse::<u32>().unwrap())
+            .sum()
+        ).collect();
+    assert_eq!(part_one(&input), 68787);
+    assert_eq!(part_two(&input), 198041);
 }
 
-fn part_one() -> u32 {
-    let top_1 = std::fs::read_to_string("input.txt")
-        .expect("Shoud read the input file")
-        .lines()
-        .fold((0, 0), |(max, cur), line| match line.parse::<u32>() {
-            Ok(val) => (max, cur + val),
-            Err(_) => (if max > cur { max } else { cur }, 0),
-        }).0;
+fn part_one(input: &Vec<u32>) -> u32 {
+    let top_1 = input
+        .iter()
+        .max()
+        .unwrap();
     println!("The fattest elf has {} calories.", top_1);
-    top_1
+    *top_1
 }
 
-fn part_two() -> u32 {
-    let mut elfs = std::fs::read_to_string("input.txt")
-        .expect("Shoud read the input file")
-        .lines()
-        .fold((Vec::new(), 0), |(mut elfs, cur), line| match line.parse::<u32>() {
-            Ok(val) => (elfs, cur + val),
-            Err(_) => {
-                elfs.push(cur);
-                (elfs, 0)
-            }
-        }).0;
+fn part_two(input: &Vec<u32>) -> u32 {
+    let mut elfs = input.clone();
     elfs.sort_by(|a, b| b.cmp(a) );
-    let top_3 = elfs.split_at(3).0.iter().fold(0, |a, b| a + b);
+    let top_3 = elfs
+        .into_iter()
+        .take(3)
+        .sum();
     println!("The three fattest elfs together has {:?} calories.", top_3);
     top_3
 }
